66a1562dbe94d4852a325b74c8bc5c793962ea01
reddisunitha <48647541+reddisunitha@users.noreply.github.com>
2024-10-02 14:59:16 +0530

    Create Employee.java

---
+++ model/Employee.java
@@ -0,0 +1,19 @@
+package com.example.employee.model;
+
+import lombok.Data;
+import javax.persistence.*;
+import java.time.LocalDate;
+import java.util.List;
+
+@Data
+@Entity
+public class Employee {
+    @Id
+    private String employeeId;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private List<String> phoneNumbers;
+    private LocalDate doj;
+    private double salary;
+}
